c7ec8fe5361502ee6e17ed1df9c83845c6cff92a
김도형 <mrdos@naver.com>
2024-12-24 13:18:35 +0900

    [도형] MainBody 수정

--- PMSProj/src/component/MainBody.jsx
+++ PMSProj/src/component/MainBody.jsx
@@ -1,28 +1,12 @@
-import MiniGames from "./MiniGameButtons";
+// import MiniGames from "./MiniGameButtons";
+import { useState } from "react";
 import PhoneList from "./PhoneList";
 
 import PropTypes from "prop-types";
 
-import { useState } from "react";
-import "../cssall/PhoneList.css"; // 드롭다운 관련 CSS 추가
-
 function MainBody({ phones, ageHandle }) {
-  const [isDropdownOpen, setDropdownOpen] = useState(false); // 드롭다운 상태 관리
-  const [selectedFilter, setSelectedFilter] = useState("모두"); // 선택된 필터값 관리
-
-  const toggleDropdown = () => {
-    setDropdownOpen(!isDropdownOpen); // 드롭다운 열기/닫기 상태 전환
-  };
-
-  const handleFilterClick = (ageGroup) => {
-    setSelectedFilter(ageGroup); // 선택된 필터값 업데이트
-    ageHandle(ageGroup); // 필터 적용
-    setDropdownOpen(false); // 드롭다운 닫기
-  };
-
   return (
     <>
-      <MiniGames />
       <div className="dropdown">
         {/* 드롭다운 메인 버튼 */}
         <button onClick={toggleDropdown} className="dropdown-toggle">

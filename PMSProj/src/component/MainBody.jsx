ec1c6a684851d161e5e52294a59db678ebc64586
lee <l_lpp@naver.com>
2024-12-24 13:16:38 +0900

    [건민]버튼/문구 중앙 이동

--- PMSProj/src/component/MainBody.jsx
+++ PMSProj/src/component/MainBody.jsx
@@ -23,19 +23,30 @@ function MainBody({ phones, ageHandle }) {
   return (
     <>
       <MiniGames />
-      <div className="dropdown">
-        {/* 드롭다운 메인 버튼 */}
-        <button onClick={toggleDropdown} className="dropdown-toggle">
-          {selectedFilter}
-        </button>
+      <div className="dropdown-container">
+        <span className="dropdown-label">나이별 인기 top3</span>
+        <div className="dropdown">
+          {/* 드롭다운 메인 버튼 */}
+          <button
+            onClick={toggleDropdown}
+            className={`dropdown-toggle ${isDropdownOpen ? "active" : ""}`}
+          >
+            {selectedFilter}
+          </button>
 
-        {/* 드롭다운 메뉴 */}
-        <div className={`dropdown-menu ${isDropdownOpen ? "show" : ""}`}>
-          {["모두", "10대", "20대", "30대", "40대", "50대"].map((ageGroup) => (
-            <button key={ageGroup} onClick={() => handleFilterClick(ageGroup)}>
-              {ageGroup}
-            </button>
-          ))}
+          {/* 드롭다운 메뉴 */}
+          <div className={`dropdown-menu ${isDropdownOpen ? "show" : ""}`}>
+            {["모두", "10대", "20대", "30대", "40대", "50대"].map(
+              (ageGroup) => (
+                <button
+                  key={ageGroup}
+                  onClick={() => handleFilterClick(ageGroup)}
+                >
+                  {ageGroup}
+                </button>
+              )
+            )}
+          </div>
         </div>
       </div>
       <PhoneList phones={phones} />

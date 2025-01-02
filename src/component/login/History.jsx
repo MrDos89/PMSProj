36e0f41a537a71b5857370cac71ba2e1c5341a0d
김도형 <mrdos@naver.com>
2025-01-02 14:53:07 +0900

    [도형] 히스토리 기록 2차 업로드

--- src/component/login/History.jsx
+++ src/component/login/History.jsx
@@ -16,22 +16,55 @@ function History({ member, onClose }) {
     );
   }
 
+  // "user_name": "홍길동",
+  // "item_id": 1,
+  // "point": 0,
+  // "remain_point": 0,
+  // "updateDate": "2024-12-30 00:00:00"
+
   return (
     <div className="history-modal">
       <div className="history-content">
         <h2>{member.name}님 기록</h2>
-        <ul className="history-list">
+        <div className="table-container">
+          <table className="history-table">
+            <thead>
+              <tr>
+                <th>날짜</th>
+                <th>이름</th>
+                <th>아이템 ID</th>
+                <th>포인트</th>
+                <th>남은 포인트</th>
+              </tr>
+            </thead>
+            <tbody>
+              {member.history.map((item, index) => (
+                <tr key={index}>
+                  <td>{item.updateDate}</td>
+                  <td>{item.user_name}</td>
+                  <td>{item.item_id}</td>
+                  <td>{item.point}</td>
+                  <td>{item.remain_point}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        {/* amount가 undefined나 null이 아닐 경우에만 표시 */}
+        {/* <ul className="history-list">
           {member.history.map((item, index) => (
             <li key={index} className="history-item">
-              <span className="history-date">{item.date}</span>
-              <span className="history-action">{item.action}</span>
+              <span className="history-date">[{item.updateDate}] </span>
+              <span className="history-action">{item.user_name} </span>
+              <span className="history-action">{item.item_id} </span>
+              <span className="history-action">{item.point} </span>
+              <span className="history-action">{item.remain_point}</span>
               {item.amount !== undefined && item.amount !== null && (
                 <span className="history-amount">{item.amount}</span>
               )}{" "}
-              {/* amount가 undefined나 null이 아닐 경우에만 표시 */}
             </li>
           ))}
-        </ul>
+        </ul> */}
         <button className="close-button" onClick={onClose}>
           닫기
         </button>
